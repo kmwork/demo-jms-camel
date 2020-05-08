29d80bbbb1af15905baa8bbf5feb1916dd5b8b1f
Konstantin Kakunin (datana) <kakunin@lanit.ru>
2020-05-08 11:42:48 +0300

    пробы

--- src/main/java/ru/datana/camel/demo/DemoKosyaCamelApp.java
+++ src/main/java/ru/datana/camel/demo/DemoKosyaCamelApp.java
@@ -1,27 +1,35 @@
 package ru.datana.camel.demo;
 
 import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
 import org.apache.camel.ProducerTemplate;
 import org.apache.camel.impl.DefaultCamelContext;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.CompletableFuture;
 
 public class DemoKosyaCamelApp {
 
-    public static void main(String... args) {
+    public static void main(String... args) throws Exception {
         CamelContext camelContext = new DefaultCamelContext();
         camelContext.start();
 
         ProducerTemplate template = camelContext.createProducerTemplate();
 
         Map<String, Object> requestHeaders = new HashMap<>();
-        String requestEndpointUri = "http://www.mocky.io/v2/5185415ba171ea3a00704eed ";
+        requestHeaders.put(Exchange.HTTP_METHOD, "GET");
+        String requestEndpointUri = "http://httpbin.org/get";
+                //"https://jsonplaceholder.typicode.com/posts/42";
+                // "https://my-json-server.typicode.com/typicode/demo/db";
+                // "http://httpbin.org/get";
+                // "http://www.mocky.io/v2/5185415ba171ea3a00704eed ";
         String reqestBody="";
-        String body = template.requestBodyAndHeaders(requestEndpointUri, reqestBody, requestHeaders, String.class);
+        CompletableFuture<String> asycBody = template.asyncRequestBodyAndHeaders(requestEndpointUri, reqestBody, requestHeaders, String.class);
 
+        while (!asycBody.isDone()) Thread.sleep(300);
 
-        System.out.println("[Kostya] body = "+body);
+        System.out.println("[Kostya] body = "+asycBody.get());
     }
 
 }
