2b6a401d96e4de58c4d32366b0f49824dc53b0b8
Konstantin Kakunin (datana) <kakunin@lanit.ru>
2020-05-13 23:33:59 +0300

    старт 4

--- src/main/java/ru/datana/camel/demo/KostyaApplication.java
+++ src/main/java/ru/datana/camel/demo/KostyaApplication.java
@@ -5,7 +5,10 @@ import org.apache.camel.Exchange;
 import org.apache.camel.ProducerTemplate;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.camel.impl.engine.SimpleCamelContext;
 import org.apache.commons.io.FileUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
@@ -19,18 +22,33 @@ import java.util.Map;
 
 @SpringBootApplication
 @Import(DatanaCamelConfig.class)
-public class KostyaApplication {
+public class KostyaApplication implements CommandLineRunner {
+
+    @Autowired
+    private CamelContext camelContext;
 
     public static void main(String[] args)  throws Exception {
         SpringApplication.run(KostyaApplication.class, args);
+
+
+
+    }
+
+    public static String readFile() throws IOException {
+        File file = new File("/home/lin/work-lanit/integration-adapter-rest-to-activemq/src/main/resources/request-example.json");
+        return FileUtils.readFileToString(file, StandardCharsets.UTF_8);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
 
         String requestEndpointUri = "http://172.29.40.42:8080/rest/getData";
         Map<String, Object> requestHeaders = new HashMap<>();
         requestHeaders.put(Exchange.HTTP_METHOD, "POST");
         requestHeaders.put(Exchange.CONTENT_TYPE, "application/json");
         String reqestBody = readFile();
-        CamelContext camelContext = new DefaultCamelContext();
-        camelContext.start();
+
         ProducerTemplate template = camelContext.createProducerTemplate();
         //Get Json from REST and Send Json to JMS
         String responseBodyJsonFromRest = template.requestBodyAndHeaders(requestEndpointUri, reqestBody, requestHeaders, String.class);
@@ -44,12 +62,6 @@ public class KostyaApplication {
             }
 
         });
-
-    }
-
-    public static String readFile() throws IOException {
-        File file = new File("/home/lin/work-lanit/integration-adapter-rest-to-activemq/src/main/resources/request-example.json");
-        return FileUtils.readFileToString(file, StandardCharsets.UTF_8);
     }
 }
 
